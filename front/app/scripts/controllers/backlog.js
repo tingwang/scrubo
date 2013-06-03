e975f145d453ba88e001b1cc513b281a662e8f38
Ting Wang <tting.wang@gmail.com>
2013-06-03 18:17:41 +0800

    Add validation.

--- front/app/scripts/controllers/backlog.js
+++ front/app/scripts/controllers/backlog.js
@@ -3,12 +3,8 @@
 angular.module('frontApp')
   .controller('BacklogCtrl', function ($scope, $http) {
       $scope.backlog = {
-        name: "test backlog",
-        storyPoints: 3,
-        description: "desc of test backlog"
       };
       $scope.create = function(e) {
-        alert('create');
         $http.post('/backlogs', $scope.backlog, function(data) {
           console.log(data);
         }, function(data) {
