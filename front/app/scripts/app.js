9bcb680a80571b71a1eed5bd55121ad820f0505c
Ting Wang <tting.wang@gmail.com>
2013-06-04 09:03:15 +0800

    add validator for checking fibonnaci.

--- front/app/scripts/app.js
+++ front/app/scripts/app.js
@@ -15,3 +15,23 @@ angular.module('frontApp', [])
         redirectTo: '/'
       });
   });
+
+angular.module('frontApp').directive('fibonnaci', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, elm, attrs, ctrl) {
+      var fibonnacis = [1, 2, 3, 5, 8, 13, 20, 40, 80];
+      ctrl.$parsers.unshift(function(viewValue) {
+        if (fibonnacis.indexOf(viewValue)) {
+          // it is valid
+          ctrl.$setValidity('fibonnaci', true);
+          return viewValue;
+        } else {
+          // it is invalid, return undefined (no model update)
+          ctrl.$setValidity('fibonnaci', false);
+          return undefined;
+        }
+      });
+    }
+  };
+});
